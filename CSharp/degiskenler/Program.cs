55c0f83ab75ef6237b6ba09645f751a0710cfed2
Mehmet Asaf ÖZTÜRK <mehmetasafozturk@gmail.com>
2022-09-02 22:11:18 +0300

    Update Program.cs

--- CSharp/degiskenler/Program.cs
+++ CSharp/degiskenler/Program.cs
@@ -57,7 +57,55 @@ namespace HelloWorld
                 Console.WriteLine("İşlem tamamlandı.");
             }
 
-            //////////////////////////////////////////////////////////////////
+            /////////////////////////////KARAR YAPILARI/////////////////////////////////////
+
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6){
+                Console.WriteLine("Günaydın");
+            }
+            else if (time>=6 && time<11) {
+                Console.WriteLine("İyi Günler");
+            }
+            else {
+                Console.WriteLine("İyi Geceler");
+            }
+
+            //Koşuş kısayolu
+            string sonuc = time<=18 ? "İyi günler" : time<10 ? "else if eklenmesi böyle" : "İyi geceler";
+            //zaman 18 den küçükse iyi günler yaz değilse iyi geceler
+
+
+            /////////////////////SWİTCH- CASE///////////////////////////
+
+            int ay = DateTime.Now.Month;
+
+            switch (ay)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Mevsim Kış");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Mevsim İlkBahar");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Mevsim Yaz");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Mevsim Sonbahar");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girdiniz");
+                    break;
+            }
         }
     }
 }
