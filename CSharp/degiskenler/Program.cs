0acef46cbe2c4f3a4da9eab3daae2b036e6d9eb4
Mehmet Asaf ÖZTÜRK <mehmetasafozturk@gmail.com>
2022-09-02 19:58:30 +0300

    a

--- CSharp/degiskenler/Program.cs
+++ CSharp/degiskenler/Program.cs
@@ -1,25 +1,63 @@
-﻿// Değişkenler Alıştırmaları
+﻿using System;
 
-int sayi1 = 46;
-int sayi2 = 8;
-string sayi3 = "20";
-float ondalıklı = 12;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Değişkenler Alıştırmaları
 
-Console.WriteLine("Birinci Sayo:" + sayi1 + " İkinci Sayı:" + sayi2);
-Console.WriteLine("Sayıların Çarpımı:" + sayi1 * sayi2);
+            int sayi1 = 46;
+            int sayi2 = 8;
+            string sayi3 = "20";
+
+            Console.WriteLine("Birinci Sayo:" + sayi1 + " İkinci Sayı:" + sayi2);
+            Console.WriteLine("Sayıların Çarpımı:" + sayi1 * sayi2);
 
 
-string isim = "Mehmet Asaf";
-string soyisim = "ÖZTÜRK";
-string isimsoyisim = isim + " " + soyisim;
+            string isim = "Mehmet Asaf";
+            string soyisim = "ÖZTÜRK";
+            string isimsoyisim = isim + " " + soyisim;
 
-Console.WriteLine("İsim Soyisim: " + isimsoyisim);
+            Console.WriteLine("İsim Soyisim: " + isimsoyisim);
 
 
-string metinsayi = string.Empty;
-metinsayi = sayi1.ToString() + sayi2.ToString();
-Console.WriteLine("Sayılar metin olarak yazılırsa: " + metinsayi);
-Console.WriteLine("Metin halindeki sayı yazırma:" + Convert.ToInt32(sayi3));
+            string metinsayi = string.Empty;
+            metinsayi = sayi1.ToString() + sayi2.ToString();
+            Console.WriteLine("Sayılar metin olarak yazılırsa: " + metinsayi);
+            Console.WriteLine("Metin halindeki sayı yazırma:" + Convert.ToInt32(sayi3));
 
-DateTime tarihsaat = DateTime.Now;
-Console.WriteLine("Tarih - Saat: " + tarihsaat);
+            DateTime tarihsaat = DateTime.Now;
+            Console.WriteLine("Tarih - Saat: " + tarihsaat);
+
+
+            /*
+            Tip dönüşümleri 2 şekilde yapılabilir:
+
+            Implicit Conversion (Bilinçsiz ya da kapalı dönüşüm)
+            Explicit Conversion (Bilinçli ya da açık dönüşüm)
+            */
+
+
+            ///////////////////////////// HATA YÖNETİMİ /////////////////////////////////
+
+            try {
+                int a = 4;
+                int b = 5;
+                int c = a+b;
+                Console.WriteLine(c);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Bir Hata Oluştu: "+ ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+
+            //////////////////////////////////////////////////////////////////
+        }
+    }
+}
