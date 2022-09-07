44cc353e40b36eb775fe58f8f35cab68117b8197
Mehmet Asaf ÖZTÜRK <mehmetasafozturk@gmail.com>
2022-09-07 17:38:31 +0300

    add

---
+++ CSharp/diziler/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace diziler
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //////////// DİZİ TANIMLAMA //////////////////
+            string[] renkler = new string[5];   //1.yöntem
+
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" }; //2.Yöntem
+
+            int[] dizi;
+            dizi = new int[5]; //3.Yöntem
+
+            //Değer Atama
+            renkler[0] = "Sarı"; //hayvanlar dizisinin 1. elemanını sarı olarak ata
+            dizi[3] = 10;
+
+
+            ///////////////////////////////// DÖNGÜLER 
+            foreach (var hayvan in hayvanlar)
+            {
+                Console.WriteLine(hayvan);
+            }
+        }
+    }
+} 
