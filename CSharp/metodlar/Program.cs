a9c6d6be0e66a12d044987a3f2269b9e2d099372
Mehmet Asaf ÖZTÜRK <mehmetasafozturk@gmail.com>
2022-09-09 21:19:24 +0300

    metodlar

---
+++ CSharp/metodlar/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+////////// İLK FONKSİYON OLUŞTURMA DENEMESİ //////////
+
+Console.WriteLine("İki adet sayı giriniz: ");
+int bir = Convert.ToInt32(Console.ReadLine());
+int iki = Convert.ToInt32(Console.ReadLine());
+
+Fonksiyonlar fonksiyon = new Fonksiyonlar();
+fonksiyon.CarpYazdir(bir, iki);
+
+
+class Fonksiyonlar
+{
+    public void CarpYazdir(int sayi1, int sayi2)
+    {
+        int sonuc = sayi1 * sayi2; 
+        Console.WriteLine(sonuc);
+    }
+}
+
+*/
+
